b3c84392d110765bdcf79da7fb533b1a4ff552da
soaresCecilia <cecilia.oliveira.soares@gmail.com>
2019-03-17 23:12:48 +0000

    Alteracoes no estabelecimento da conexao

--- client.py
+++ client.py
@@ -1,63 +1,89 @@
 from socket import *
-from struct import *
-import time
 
-serverName = '127.0.0.1'
+import json
 
-serverPort = 12000
+
 
 timeout = 0.003
 
 
 
 #função que estabelece os requisitos que o cliente envia ao servidor para estabelecer a conexão
-def startConnectionRequest(username, password, action, fileName, attempt):
-    client = {}
-    client["username"] = username
-    client["password"] = password
-    client["action"] = action
-    client["fileName"] = fileName
-    client["attempt"] = attempt + 1
+def startConnection(serverName, serverPort, username, password, action, fileName, attempt):
+    connectionMessage = {}
+    connectionMessage["username"] = username
+    connectionMessage["password"] = password
+    connectionMessage["action"] = action #upload ou download
+    connectionMessage["fileName"] = fileName
+    connectionMessage["attempt"] = attempt + 1
 
-    return client
+
+    clientSocket = socket(AF_INET, SOCK_DGRAM) #cria um socket UDP
+
+    totalSegments = 0
+
+
+    connection = {
+        'clientsocket': clientSocket,
+        'serverPort': serverPort,
+        'serverName': serverName,
+        'totalSegments': totalSegments
+    }
+
+
+    sendMessage(connection, connectionMessage) #envia a mensagem de ligação
+
+    synAck = recvMessage(connection)
+
+    connection['totalSegments'] = synAck['totalSegments']
+
+
+    return connection
+
+
+
+#recebe mensagem
+def recvMessage(connection):
+   newMessage, serverAddress = connection['clientsocket'].recvfrom(2048)
+
+   decodeMessage = json.loads(newMessage.decode())
+
+   return decodeMessage
+
+
+
+#envia mensagem
+def sendMessage(connection, message):
+    bytesMessage = json.dumps(message).encode()
+
+    connection["clientsocket"].sendto(bytesMessage, (connection["serverName"], connection["serverPort"]))
+
 
 
 #função que tenta novamente estabelecer a ligação caso não haja resposta do servidor
-def retryConnection(client):
+def retryConnection(connection, connectionMessage):
 
-    if timeout > 0.003 and client["attempt"] <= 3:
-        startConnectionRequest(client["username"], client["password"], client["action"], client["fileName"], client["attempt"])
+    if timeout > 0.003 and connectionMessage["attempt"] <= 3:
+        startConnection(connection["serverName"], connection["serverPort"], connectionMessage["username"],
+                        connectionMessage["password"], connectionMessage["action"],
+                        connectionMessage["fileName"], connectionMessage["attempt"])
 
     else:
         return -1
 
-    return client
+    return connection
 
 
-#cria o cabeçalho dos segmentos
-def header(checksum, ack, n_sequence):
-    head = {}
-    head["checksum"] = checksum
-    if ack == 3:
-        lista = [] # vai ter função do estado
-        head["ack"] = "3," +  ",".join(str(x) for x in lista)
-    else:
-        head["ack"] = str(ack)
 
-    head["n_sequence"] = n_sequence
-
-    return header
+def closeConnection(connection):
+    connection["clientsocket"].close()
 
 
 
 
 
-clientSocket = socket(AF_INET, SOCK_DGRAM)  # tipo de conexão e protocolo de transporte - UDP
-message = input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(), (serverName,
-                                           serverPort))  # converte a string para byte -enconde - send to - cola o destino à mensagem e manda o resultado para clientSocket
-modifiedMessage, serverAddress = clientSocket.recvfrom(
-        2048)  # recebe uma mensagem da rede no máximo de 2048 bytes qdo um pacote chega da internet ao socket do cliente os dados do pacote vão para a variável modifeidMessage e o endereço donde veio o pacote vai para serverAddress
-print(modifiedMessage.decode())  # imprime a mensagem enviada
-clientSocket.close()  # fecha a conexão
 
+connection = startConnection("127.0.0.1", 12000, "Luis", "123", "download", "bicho", 1)
+print(connection)
+
+closeConnection(connection)
--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 from socket import *
 
 
+import json
+
+
 serverPort = 12000
 
 PDU = 548
 
-HEADBYTES = 20 #tem de ser fixo para se poder calcular a quantidade de pacotes
+HEADBYTES = 20 #tem de ser fixo para se poder calcular a quantidade de pacotes, mas não precisa de ser 20
+
 
 #conta o número de segmentos que vão ser enviados
-def n_Segments(data):
+def total_Segments(data):
     if (data % (PDU + HEADBYTES)) != 0:
         segments = (data / PDU) + 1
     else:
@@ -17,16 +21,50 @@ def n_Segments(data):
     return segments
 
 
+#cria o cabeçalho dos segmentos
+def header(checksum, ack, n_sequence):
+    head = {}
+    head['checksum'] = checksum
+    if ack == 3:
+        lista = [] # vai ter função do estado
+        head['ack'] = "3," + ",".join(str(x) for x in lista)
+    else:
+        head['ack'] = str(ack)
+
+    head['n_sequence'] = n_sequence
+
+    return head
+
+
+
+#inicio da conexão
+def startServer():
+
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(('', serverPort))
+
+    totalSegments = 0
+    firstSegment = 1
+
+    synAck = {
+        'totalSegments': totalSegments,
+        'firstSegment': firstSegment,
+    }
+
+    synMessage, clientAddress = serverSocket.recvfrom(2048)
+
+    decodeMessage = json.loads(synMessage.decode())
+
+
+    bytesMessage = json.dumps(synAck).encode()
+
+    serverSocket.sendto(bytesMessage, clientAddress)
+
+
+    return serverSocket
 
 
 
 
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))  # atribui a porta 12000 ao socket do servidor
-print("The server is ready to receive")
-while True:
-    message, clientAddress = serverSocket.recvfrom(
-           2048)  # qdo um pacote chega da internet ao socket do server os dados do pacote vão para message e end donde veio o pacote vai para clientAddress
-    modifiedMessage = message.decode().upper()  # converte a mensagem numa string e depois passa para maiusculas
-    serverSocket.sendto(modifiedMessage.encode(),
-                            clientAddress)  # envia o segmento com o endereço do cliente colado aos dados para o cliente
+
+serverSocket = startServer()
