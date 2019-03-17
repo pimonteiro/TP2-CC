20a1086e2562aa23346c2024ebfaa0b82356f719
soaresCecilia <cecilia.oliveira.soares@gmail.com>
2019-03-17 21:23:02 +0000

    Primeiros passos no estabelecimento da conexao

---
+++ client.py
@@ -0,0 +1,85 @@
+from socket import *
+
+import json
+
+timeout = 0.003
+
+
+
+#função que estabelece os requisitos que o cliente envia ao servidor para estabelecer a conexão
+def startConnection(serverName, serverPort, username, password, action, fileName, attempt):
+    connectionMessage = {}
+    connectionMessage["username"] = username
+    connectionMessage["password"] = password
+    connectionMessage["action"] = action #upload ou download
+    connectionMessage["fileName"] = fileName
+    connectionMessage["attempt"] = attempt + 1
+
+
+    clientSocket = socket(AF_INET, SOCK_DGRAM) #cria um socket UDP
+
+    totalSegments = 0
+
+    connection = {
+        'clientsocket': clientSocket,
+        'serverPort': serverPort,
+        'serverName': serverName,
+        'totalSegments': totalSegments
+    }
+
+
+    sendMessage(connection, connectionMessage) #envia a mensagem de ligação
+
+    synAck = recvMessage(connection)
+
+    connection['totalSegments'] = synAck['totalSegments']
+
+    return connection
+
+
+
+#recebe mensagem
+def recvMessage(connection):
+   newMessage, serverAddress = connection['clientsocket'].recvfrom(2048)
+
+   decodeMessage = json.loads(newMessage.decode())
+
+   return decodeMessage
+
+
+
+#envia mensagem
+def sendMessage(connection, message):
+    bytesMessage = json.dumps(message).encode()
+
+    connection["clientsocket"].sendto(bytesMessage, (connection["serverName"], connection["serverPort"]))
+
+
+
+#função que tenta novamente estabelecer a ligação caso não haja resposta do servidor
+def retryConnection(connection, connectionMessage):
+
+    if timeout > 0.003 and connectionMessage["attempt"] <= 3:
+        startConnection(connection["serverName"], connection["serverPort"], connectionMessage["username"],
+                        connectionMessage["password"], connectionMessage["action"],
+                        connectionMessage["fileName"], connectionMessage["attempt"])
+
+    else:
+        return -1
+
+    return connection
+
+
+
+
+def closeConnection(connection):
+    connection["clientsocket"].close()
+
+
+connection = startConnection("127.0.0.1", 12000, "Luis", "123", "download", "bicho", 1)
+print(connection)
+
+closeConnection(connection)
+
+
+
---
+++ server.py
@@ -0,0 +1,67 @@
+from socket import *
+
+
+import json
+
+
+serverPort = 12000
+
+PDU = 548
+
+HEADBYTES = 20 #tem de ser fixo para se poder calcular a quantidade de pacotes, mas não precisa de ser 20
+
+#conta o número de segmentos que vão ser enviados
+def total_Segments(data):
+    if (data % (PDU + HEADBYTES)) != 0:
+        segments = (data / PDU) + 1
+    else:
+        segments = data / PDU
+
+    return segments
+
+
+#cria o cabeçalho dos segmentos
+def header(checksum, ack, n_sequence):
+    head = {}
+    head["checksum"] = checksum
+    if ack == 3:
+        lista = [] # vai ter função do estado
+        head["ack"] = "3," + ",".join(str(x) for x in lista)
+    else:
+        head["ack"] = str(ack)
+
+    head["n_sequence"] = n_sequence
+
+    return header
+
+#inicio da conexão
+def startServer():
+
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(('', serverPort))
+
+    totalSegments = 0
+    firstSegment = 1
+
+    synAck = {
+        'totalSegments': totalSegments,
+        'firstSegment': firstSegment
+    }
+
+    synMessage, clientAddress = serverSocket.recvfrom(2048)
+
+    decodeMessage = json.loads(synMessage.decode())
+
+
+    bytesMessage = json.dumps(synAck).encode()
+
+    serverSocket.sendto(bytesMessage, clientAddress)
+
+
+    return serverSocket
+
+
+
+serverSocket = startServer()
+
+
